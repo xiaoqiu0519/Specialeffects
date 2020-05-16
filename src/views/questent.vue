2f2c2dfc0c6d8dd3c9e07b435549aaad6113331a
cage <cage@vision-fund.cc>
2020-05-16 19:11:12 +0800

    新增功能

--- src/views/questent.vue
+++ src/views/questent.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="questent">
-    
+    <div class="con con1">
+      
+    </div>
   </div>
 </template>
 <script>
 export default {
   data(){
-    
+
   }
 }
 </script>
 <style lang="stylus" scoped>
+.questent
+  width 1000px;
+  margin 0 auto;
+  .con
+    width 1000px;
+    height 500px;
 
 </style>
